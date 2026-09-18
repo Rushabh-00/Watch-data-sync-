24242fee78981f5453b109e7a9c303c61e57c88a
Rushabh <161584263+Rushabh-00@users.noreply.github.com>
2026-09-18 19:48:01 +0530

    Add synced health data models

---
+++ android/app/src/main/java/app/watchdatasync/model/HealthData.kt
@@ -0,0 +1,20 @@
+package app.watchdatasync.model
+
+data class HeartRateHistorySample(
+    val epochMillis: Long,
+    val bpm: Int,
+)
+
+data class Spo2HistorySample(
+    val epochMillis: Long,
+    val percent: Int,
+)
+
+data class DailyActivitySummary(
+    val epochMillis: Long,
+    val steps: Int,
+    val calories: Int,
+    val distanceMeters: Int,
+    val activeMinutes: Int,
+)
+
